4af3fe8000f8c353debbd1571c1f6ea91034c8da
EngBadr <bmuzini@gmail.com>
2021-01-07 19:30:17 +0300

    Add files via upload

---
+++ floatAddtion.py
@@ -0,0 +1,4 @@
+fnum1=float(input("Enter 1st Float Number FN1 : "))
+fnum2=float(input("Enter 2nd Float Number FN2 : "))
+
+print("FN1+FN2 =", fnum1 + fnum2)
---
+++ welcome.py
@@ -0,0 +1,2 @@
+userName=input("Please Enter Your UserName!\n")
+print('Welcome', userName)
